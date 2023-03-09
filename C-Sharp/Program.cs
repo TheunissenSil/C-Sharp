7a85b1cea26de7b3cdc502b3dd01e5ded1b009a9
TheunissenSil <theunissensil@gmail.com>
2023-03-09 14:03:37 +0100

    Opdracht 3-21

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -20,6 +20,11 @@
             words.ForEach(Console.WriteLine);
 
             // ----------------------------------------------
+            // Exercise 3-21 --------------------------------
+            int[] array = { 5, 1, 3, 4, 2 };
+            PrintArrayInStars(array);
+
+            // ----------------------------------------------
         }
 
         public static void RemoveLast(List<string> words)
@@ -28,6 +33,16 @@
             if (words.Count > 0)
             {
                 words.RemoveAt(words.Count - 1);    
+            }
+        }
+
+        public static void PrintArrayInStars(int[] array)
+        {
+            // Exercise 3-21
+            for (int i = 0; i < array.Length; i++)
+            {
+                String stars = new String('*', array[i]);
+                Console.WriteLine(stars);
             }
         }
 
