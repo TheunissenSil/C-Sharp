1e74c2b91a849f6e9f8714b9151bf2a80d85b284
TheunissenSil <theunissensil@gmail.com>
2023-03-09 13:48:27 +0100

    Opdracht 3-16

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -4,9 +4,32 @@
     {
         static void Main(string[] args)
         {
-            
+            // Exercise 3-16 --------------------------------
+            List<string> words = new List<string>();
+
+            words.Add("First");
+            words.Add("Second");
+            words.Add("Third");
+
+            // Remember, this is how you print all the items in a list
+            words.ForEach(Console.WriteLine);
+
+            RemoveLast(words);
+            RemoveLast(words);
+
+            words.ForEach(Console.WriteLine);
+
+            // ----------------------------------------------
         }
 
+        public static void RemoveLast(List<string> words)
+        {
+            // Exercise 3-16
+            if (words.Count > 0)
+            {
+                words.RemoveAt(words.Count - 1);    
+            }
+        }
 
     }
 }
