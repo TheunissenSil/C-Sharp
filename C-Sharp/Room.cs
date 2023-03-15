15f8e10e57507098055a365e570fb5e614748384
TheunissenSil <theunissensil@gmail.com>
2023-03-15 09:23:51 +0100

    Opdracht 4-4

---
+++ C-Sharp/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Room
+    {
+        private string code;
+        private int seats;
+
+        public Room(string classCode, int numberOfSeats)
+        {
+            code = classCode;
+            seats = numberOfSeats;
+        }   
+
+    }
+}
