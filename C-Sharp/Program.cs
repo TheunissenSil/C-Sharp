3ce864e5d7d526ce6e514707f90405d30ebf5e3b
TheunissenSil <theunissensil@gmail.com>
2023-03-15 08:53:40 +0100

    Opdracht 3-30

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -5,23 +5,23 @@
         static void Main(string[] args)
         {
             // Exercise 3-16 --------------------------------
-            List<string> words = new List<string>();
+            //List<string> words = new List<string>();
 
-            words.Add("First");
-            words.Add("Second");
-            words.Add("Third");
+            //words.Add("First");
+            //words.Add("Second");
+            //words.Add("Third");
 
-            words.ForEach(Console.WriteLine);
+            //words.ForEach(Console.WriteLine);
 
-            RemoveLast(words);
-            RemoveLast(words);
+            //RemoveLast(words);
+            //RemoveLast(words);
 
-            words.ForEach(Console.WriteLine);
+            //words.ForEach(Console.WriteLine);
 
             // ----------------------------------------------
             // Exercise 3-21 --------------------------------
-            int[] array = { 5, 1, 3, 4, 2 };
-            PrintArrayInStars(array);
+            //int[] array = { 5, 1, 3, 4, 2 };
+            //PrintArrayInStars(array);
 
             // ----------------------------------------------
             // Exercise 3-28 --------------------------------
@@ -29,7 +29,11 @@
 
             // ----------------------------------------------
             // Exercise 3-29 --------------------------------
-            Opd3_29();
+            //Opd3_29();
+
+            // ----------------------------------------------
+            // Exercise 3-30 --------------------------------
+            Opd3_30();
 
             // ----------------------------------------------
         }
@@ -110,6 +114,48 @@
             }
 
             Console.WriteLine("Age of the oldest: " + maxAge);
+        }
+
+        public static void Opd3_30()
+        {
+            String name = "";
+            int maxAge = 0;
+            bool isFirstInput = true;
+
+            while (true)
+            {
+                Console.Write("Enter name and age (or empty line to quit): ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                else
+                {
+                    string[] parts = input.Split(',');
+                    if (parts.Length == 2 && int.TryParse(parts[1], out int age))
+                    {
+                        Console.WriteLine("Name: " + parts[0] + ", Age: " + age);
+                        if (isFirstInput)
+                        {
+                            name = parts[0];
+                            maxAge = age;
+                            isFirstInput = false;
+                        }
+                        else if (age > maxAge)
+                        {
+                            name = parts[0];
+                            maxAge = age;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter name and age separated by a comma.");
+                    }
+                }
+            }
+
+            Console.WriteLine("Name of the oldest: " + name);
         }
     }
 }
